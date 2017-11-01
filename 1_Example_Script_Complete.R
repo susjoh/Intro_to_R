fb60dcf23efd7ef3c86457d4b3e7b701f241c038
Susan Johnston <Susan.Johnston@ed.ac.uk>
2017-11-01 17:26:02 +0000

    Update

--- 1_Example_Script_Complete.R
+++ 1_Example_Script_Complete.R
@@ -11,17 +11,30 @@
 # 0. Warming up
 #----------------------------------------
 
+# Exercise 1
 
+2+3
+1:10
+seq(1, 20, 4)
+mean(c(3, 6, 9, 3, 6, 7))
 
+x <- 1:10
+x
 
+y <- seq(0, 4.5, 0.5)
+y
 
+# Exercise 2
 
+??"standard deviation"
+sort(x, decreasing = T)
+sort(y, decreasing = T)
 
 #----------------------------------------
 # 1. Set up the working environment
 #----------------------------------------
 
-
+library(dplyr)
 
 
 
@@ -32,7 +45,8 @@
 # 2. Load data
 #----------------------------------------
 
-
+# Peru_Soil_Data <- read.delim("C:/Users/sjohns10/Google Drive/Teaching and Seminars/201711 NERC E3 DTP/Intro_to_R/data/Peru_Soil_Data.txt")
+Peru_Soil_Data <- read.table("data/Peru_Soil_Data.txt", header = T)  # Exercise 3
 
 
 
